383c872efd32d77e7dd4f585e4e77b030cb0939e
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-12 22:48:46 +0200

    Update setup files, add typing

--- setup.py
+++ setup.py
@@ -1,50 +1,56 @@
-from setuptools import setup, find_packages
-from os import path
+#!/usr/bin/env python3
 
 import platform
+from pathlib import Path
 
-modules = []
-packages = [
-    "vsgenstubs", "vsgenstubs4"
-]
-requirements = []
-entrypoints = [
-    "vsgenstubs=vsgenstubs:main",
-    "vsgenstubs4=vsgenstubs4:main",
-]
+import setuptools
 
-if platform.platform().startswith("Windows"):
-    modules.extend(["vsrepo", "vsrupdate"])
-    entrypoints.extend([
-        "vsrepo=vsrepo:noop",
-        "vsrupdate=vsrupdate:noop"
-    ])
-    requirements.append("tqdm")
+package_name = 'vsrepo'
 
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+is_windows = platform.platform().startswith('Windows')
 
-setup(
-    name="vsrepo",
-    version="50",
-    description="A simple package repository for VapourSynth.",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url="http://www.vapoursynth.com/",
-    author = "Myrsloik",
-    packages=packages,
-    py_modules = modules,
+packages = ['vsgenstubs', 'vsgenstubs4']
+modules = requirements = []
+
+if is_windows:
+    modules = ['vsrepo', 'vsrupdate']
+    requirements = ['tqdm']
+
+entrypoints = [*packages, *modules]
+
+
+setuptools.setup(
+    name=package_name,
+    version='50',
+    author='Myrsloik',
+    author_email='fredrik.mellbin@gmail.com',
+    description='A simple package repository for VapourSynth',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://www.vapoursynth.com/',
+    project_urls={
+        'Issues': 'https://github.com/vapoursynth/vsrepo/issues',
+        'Source': 'https://github.com/vapoursynth/vsrepo'
+    },
     install_requires=requirements,
+    python_requires='>=3.8',
+    py_modules=modules,
+    packages=[
+        package_name
+    ],
     include_package_data=True,
     package_data={
-        package: ["*.pyi"] for package in packages
+        package: ['*.pyi', 'py.typed'] for package in packages
     },
-    entry_points = {
-        'console_scripts': entrypoints
-    },
-    project_urls={
-        "Issues": "https://github.com/vapoursynth/vsrepo/issues",
-        "Source": "https://github.com/vapoursynth/vsrepo"
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    entry_points={
+        'console_scripts': [
+            f'{module}={module}:main'
+            for module in entrypoints
+        ]
     }
 )

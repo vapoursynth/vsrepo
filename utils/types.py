09ba19c71207b2910828985fbb48f7f84296fea6
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-13 02:01:33 +0200

    Update, fix and simplify Descriptors, typing

--- utils/types.py
+++ utils/types.py
@@ -1,33 +1,35 @@
-from dataclasses import dataclass, field
-from enum import Enum
 import json
 import logging
-from pathlib import Path
 import sys
-from typing import Dict, Generic, Iterator, List, NamedTuple, Tuple, Type, TypeVar, TypedDict, Union
+from dataclasses import dataclass, field
+from enum import Enum
+from pathlib import Path
+from typing import Dict, Generic, Iterator, List, NamedTuple, Type, TypedDict, TypeVar, Union
 
 T = TypeVar('T')
-VT = TypeVar('VT')
 
 
-class DescriptorBase(Generic[VT, T]):
+class DescriptorBase(Generic[T]):
     cls_type: Type[T]
 
-    def __class_getitem__(cls, new_cls_type: Tuple[Type[VT], Type[T]]) -> 'Type[DescriptorBase[VT, T]]':
+    def __class_getitem__(cls, new_cls_type: Type[T]) -> 'DescriptorBase[T]':
         class inner_Descriptor(cls):  # type: ignore
-            @property
-            def cls_type(cls) -> Type[T]:
-                if not hasattr(cls, '_cls_type'):
-                    cls._cls_type = eval(new_cls_type[-1])  # type: ignore
-                return cls._cls_type  # type: ignore
+            if isinstance(new_cls_type, type):
+                cls_type = new_cls_type
+            else:
+                @property  # type: ignore
+                def cls_type(cls) -> Type[T]:
+                    if not hasattr(cls, '_cls_type'):
+                        cls._cls_type = eval(new_cls_type)
+                    return cls._cls_type
 
-        return inner_Descriptor
+        return inner_Descriptor  # type: ignore
 
-    def __init__(self, *, default: T) -> None:
+    def __init__(self, default: T) -> None:
         self._default = default
 
     def __set_name__(self, owner: object, name: str) -> None:
-        self._name = "_" + name
+        self._name = '_' + name
 
     def __get__(self, obj: object, ctype: type) -> T:
         if obj is None:
@@ -35,35 +37,32 @@ class DescriptorBase(Generic[VT, T]):
 
         return getattr(obj, self._name, self._default)
 
-    def __set__(self, obj: object, value: Union[VT, T]) -> None:
+    def __set__(self, obj: object, value: T) -> None:
         setattr(obj, self._name, self.cls_type(value))  # type: ignore
 
 
-class VSPackageUpdateMode(str, Enum):
+class CustomEnum(Enum):
+    @classmethod
+    def Descriptor(cls: Type[T], default: T) -> T:
+        return DescriptorBase[cls](default)  # type: ignore
+
+
+class VSPackageUpdateMode(str, CustomEnum):
     MANUAL = 'manual'
     GIT = 'git-commits'
 
-    class Descriptor(DescriptorBase[str, 'VSPackageUpdateMode']):
-        ...
 
-
-class VSPackageDeviceType(str, Enum):
+class VSPackageDeviceType(str, CustomEnum):
     CPU = 'cpu'
     CUDA = 'cuda'
     OPENCL = 'opencl'
     VULKAN = 'vulkan'
 
-    class Descriptor(DescriptorBase[str, 'VSPackageDeviceType']):
-        ...
 
-
-class VSPackageType(str, Enum):
+class VSPackageType(str, CustomEnum):
     SCRIPT = 'PyScript'
     WHEEL = 'PyWheel'
     PLUGIN = 'VSPlugin'
-
-    class Descriptor(DescriptorBase[str, 'VSPackageType']):
-        ...
 
     def get_package_key(self, n_bits: int = 64) -> str:
         if self is VSPackageType.SCRIPT:
@@ -138,8 +137,8 @@ class VSPackage:
     website: str = ''
     doom9: str = ''
     api: int = 3
-    type: VSPackageType.Descriptor = VSPackageType.Descriptor(default=VSPackageType.SCRIPT)
-    updatemode: VSPackageUpdateMode.Descriptor = VSPackageUpdateMode.Descriptor(default=VSPackageUpdateMode.MANUAL)
+    type: VSPackageType = VSPackageType.Descriptor(VSPackageType.SCRIPT)
+    updatemode: VSPackageUpdateMode = VSPackageUpdateMode.Descriptor(VSPackageUpdateMode.MANUAL)
     releases: List[VSPackageRelease] = field(default_factory=list)
     dependencies: List[str] = field(default_factory=list)
     device: List[VSPackageDeviceType] = field(default_factory=list)

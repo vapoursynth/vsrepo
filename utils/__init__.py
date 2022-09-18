b8135376b016e8594b38776a5e308f550c618b2b
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-18 22:10:26 +0200

    Add utils.net

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
+from .net import *  # noqa: F401, F403
 from .types import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
---
+++ utils/net.py
@@ -0,0 +1,38 @@
+
+from typing import Dict, Union, cast
+from urllib.request import urlopen
+
+try:
+    import tqdm  # type: ignore
+    tqdm_available = True
+except ImportError:
+    tqdm_available = False
+
+
+def fetch_url(url: str, desc: Union[str, None] = None) -> bytearray:
+    with urlopen(url) as urlreq:
+        if tqdm_available and (urlreq.headers['content-length'] is not None):
+            size = int(urlreq.headers['content-length'])
+            remaining = size
+            data = bytearray()
+            with tqdm.tqdm(total=size, unit='B', unit_scale=True, unit_divisor=1024, desc=desc) as t:
+                while remaining > 0:
+                    blocksize = min(remaining, 1024 * 128)
+                    data.extend(urlreq.read(blocksize))
+                    remaining = remaining - blocksize
+                    t.update(blocksize)
+            return data
+        else:
+            print('Fetching: ' + url)
+            return cast(bytearray, urlreq.read())
+
+
+download_cache: Dict[str, bytearray] = {}
+
+
+def fetch_url_cached(url: str, desc: str = "") -> bytearray:
+    data = download_cache.get(url, None)
+    if data is None:
+        data = fetch_url(url, desc)
+        download_cache[url] = data
+    return data

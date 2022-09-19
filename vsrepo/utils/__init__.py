637af524eeea336fbad0e1b65325d930c798bdef
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-19 22:43:58 +0200

    Move utils

---
+++ vsrepo/utils/__init__.py
@@ -0,0 +1,7 @@
+from .install import *  # noqa: F401, F403
+from .installations import *  # noqa: F401, F403
+from .net import *  # noqa: F401, F403
+from .packages import *  # noqa: F401, F403
+from .site import *  # noqa: F401, F403
+from .types import *  # noqa: F401, F403
+from .utils import *  # noqa: F401, F403

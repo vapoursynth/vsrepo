4421303de89817b63484b4f5a4ce0327511bbae1
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-19 15:39:16 +0200

    Remove last bits of os.path

--- utils/site.py
+++ utils/site.py
@@ -1,10 +1,7 @@
 from pathlib import Path
 import sys
 from argparse import Namespace
-from os import getcwd, getenv, makedirs
-from os.path import abspath, dirname, isfile
-from os.path import join as join_path
-from os.path import pardir
+from os import getenv
 from typing import NamedTuple, Optional, Union
 
 from .installations import detect_vapoursynth_installation, is_sitepackage_install, is_sitepackage_install_portable
@@ -20,16 +17,16 @@ def is_venv() -> bool:
     return hasattr(sys, "real_prefix") or (hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix)
 
 
-def get_vs_installation_site() -> str:
+def get_vs_installation_site() -> Path:
     if is_venv():
         try:
-            return dirname(detect_vapoursynth_installation())
+            return detect_vapoursynth_installation().parent
         except ImportError:
             import setuptools
-            return dirname(dirname(setuptools.__file__))
+            return Path(setuptools.__file__).parent.parent
 
     import site
-    return site.getusersitepackages()
+    return Path(site.getusersitepackages())
 
 
 class InstallationInfo(NamedTuple):
@@ -54,72 +51,69 @@ class InstallationInfo(NamedTuple):
 
 
 def get_installation_info(args: Namespace) -> InstallationInfo:
-    is_64bits = args.target == 'win64'
+    is_64bits = str(args.target) == 'win64'
 
-    file_dirname = dirname(join_path(pardir, pardir, abspath(__file__)))
+    file_dirname = Path(__file__).parent.parent.absolute()
 
     # VSRepo is installed to the site-packages.
-    if abspath(file_dirname).startswith(abspath(sys.prefix)):
-        file_dirname = getcwd()
+    if Path(sys.prefix) in file_dirname.parents:
+        file_dirname = Path.cwd()
 
     if args.portable:
-        plugin32_path = join_path(file_dirname, 'vapoursynth32', 'plugins')
-        plugin64_path = join_path(file_dirname, 'vapoursynth64', 'plugins')
-        package_json_path = join_path(file_dirname, 'vspackages3.json')
+        plugin32_path = file_dirname / 'vapoursynth32' / 'plugins'
+        plugin64_path = file_dirname / 'vapoursynth64' / 'plugins'
+        package_json_path = file_dirname / 'vspackages3.json'
     elif is_sitepackage_install_portable(args.portable):
-        vapoursynth_path = detect_vapoursynth_installation()
-        base_path = dirname(vapoursynth_path)
-        plugin32_path = join_path(base_path, 'vapoursynth32', 'plugins')
-        plugin64_path = join_path(base_path, 'vapoursynth64', 'plugins')
-        package_json_path = join_path(base_path, 'vspackages3.json')
-        del vapoursynth_path
-    else:
-        pluginparent = [str(getenv('APPDATA')), 'VapourSynth']
-        plugin32_path = join_path(*pluginparent, 'plugins32')
-        plugin64_path = join_path(*pluginparent, 'plugins64')
-        package_json_path = join_path(*pluginparent, 'vsrepo', 'vspackages3.json')
+        base_path = detect_vapoursynth_installation().parent
 
-    plugin_path: str = plugin64_path if is_64bits else plugin32_path
+        plugin32_path = base_path / 'vapoursynth32' / 'plugins'
+        plugin64_path = base_path / 'vapoursynth64' / 'plugins'
+        package_json_path = base_path / 'vspackages3.json'
+    else:
+        pluginparent = Path(str(getenv('APPDATA'))) / 'VapourSynth'
+
+        plugin32_path = pluginparent / 'plugins32'
+        plugin64_path = pluginparent / 'plugins64'
+        package_json_path = pluginparent / 'vsrepo' / 'vspackages3.json'
+
+    plugin_path = plugin64_path if is_64bits else plugin32_path
 
     if args.force_dist_info or is_sitepackage_install(args.portable):
         if is_venv():
             try:
                 import setuptools
-                site_package_dir: Optional[str] = dirname(dirname(setuptools.__file__))
+                site_package_dir: Optional[Path] = Path(setuptools.__file__).parent.parent
                 del setuptools
             except ImportError:
                 site_package_dir = None
         else:
             import site
-            site_package_dir = site.getusersitepackages()
+            site_package_dir = Path(site.getusersitepackages())
     else:
         site_package_dir = None
 
-    py_script_path: str = file_dirname if args.portable else (
-        site_package_dir if site_package_dir is not None else get_vs_installation_site()
-    )
+    py_script_path = file_dirname if args.portable else (site_package_dir or get_vs_installation_site())
+
     if args.script_path is not None:
-        py_script_path = args.script_path
+        py_script_path = Path(args.script_path)
 
     if args.binary_path is not None:
-        plugin_path = args.binary_path
+        plugin_path = Path(args.binary_path)
 
-    makedirs(py_script_path, exist_ok=True)
-    makedirs(plugin_path, exist_ok=True)
-    makedirs(dirname(package_json_path), exist_ok=True)
+    py_script_path.mkdir(parents=True, exist_ok=True)
+    plugin_path.mkdir(parents=True, exist_ok=True)
+    package_json_path.parent.mkdir(parents=True, exist_ok=True)
 
-    cmd7zip_path: str = join_path(file_dirname, '7z.exe')
-    if not isfile(cmd7zip_path):
+    cmd7zip_path = file_dirname / '7z.exe'
+    if not cmd7zip_path.is_file():
         try:
             with winreg.OpenKeyEx(
                 winreg.HKEY_LOCAL_MACHINE, 'SOFTWARE\\7-Zip', reserved=0, access=winreg.KEY_READ
             ) as regkey:
-                cmd7zip_path = join_path(winreg.QueryValueEx(regkey, 'Path')[0], '7z.exe')
+                cmd7zip_path = Path(winreg.QueryValueEx(regkey, 'Path')[0]) / '7z.exe'
         except BaseException:
-            cmd7zip_path = '7z.exe'
+            cmd7zip_path = Path('7z.exe')
 
     return InstallationInfo(
-        is_64bits, Path(file_dirname), Path(package_json_path), Path(plugin_path),
-        site_package_dir and Path(site_package_dir) or None,
-        Path(py_script_path), Path(cmd7zip_path)
+        is_64bits, file_dirname, package_json_path, plugin_path, site_package_dir, py_script_path, cmd7zip_path
     )
